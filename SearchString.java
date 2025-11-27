424e5f00bedd38c8adcb659e92325e6f61c40994
Ida-Leefjelstrenius <ida-lee.fjelstrenius@hotmail.se>
2025-11-27 13:48:01 +0100

    added SearchString.java file and a test textfile to use the program

--- SearchString.java
+++ SearchString.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SearchString {
 
     public static void main(String[] args) {
-        
+        Scanner userScanner = new Scanner(System.in);
+        String input = userScanner.nextLine();
+
+        String[] inputParts= input.split(" ");
+
+        if (inputParts.length != 3) {
+            System.out.println("Please give the right input format: search<pattern> <file>");
+            return;
+        }
+
+        String pattern = inputParts[1];
+        String fileName = inputParts[2];
+
+        File file = new File(fileName);
+
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                String fileLine = fileScanner.nextLine();
+                if (fileLine.contains(pattern)) {
+                    System.out.println(fileLine);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open the file");
+        }   
     }
     
 }
